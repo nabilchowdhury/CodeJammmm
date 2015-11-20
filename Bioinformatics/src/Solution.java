07875dded4903033b08deeb4bbc9e5ff80719dc5
SebastianPilarski <sebastian.pilarski@mail.mcgill.ca>
2015-11-19 23:28:46 -0800

    Init prestuff

--- Bioinformatics/src/Solution.java
+++ Bioinformatics/src/Solution.java
@@ -1,6 +1,12 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
-	public static void main(String[] args){
-		System.out.println("we gon win");
-	}
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
 }
