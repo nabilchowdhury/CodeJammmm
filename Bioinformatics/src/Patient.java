90c6b80e881e8007ba4a187ba640874a335f43ab
Nabil <nabillionaire@live.com>
2015-11-20 23:33:11 -0500

    average method

--- Bioinformatics/src/Patient.java
+++ Bioinformatics/src/Patient.java
@@ -2,13 +2,7 @@
 public class Patient {
 	public String[] properties;
 	
-	public Patient(String s){
-		/*String[] temp = s.split(" ");
-		for(int i=0; i<temp.length; i++){
-			properties[i] = temp[i];
-			System.out.println(properties[i]);
-		}*/
-		
+	public Patient(String s){	
 		properties = s.split("	");
 	}
 }
